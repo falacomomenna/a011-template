55eb280ea810953f5f6112688ebd8a1899c7bf55
falacomomenna <danielmenna@gmail.com>
2022-08-01 20:00:43 -0300

    ativdades da aula até antes do intervalo

--- script.js
+++ script.js
@@ -10,11 +10,30 @@ const estudante = {
 
 console.log(estudante)
 
+//criação de nova propriedade do objeto
 estudante.modulo = 'git e terminal'
 console.log(estudante)
 
-console.log(estudante.nome, estudante.notasDoSemestre[1], estudante.modulo)
-
+//acessando propriedades do objeto
 console.log('NOME:', estudante.nome)
 console.log('SEGUNDA NOTA: ' + estudante.notasDoSemestre[1])
+//outra forma de notação para acessar as propriedades do objeto
 console.log('MÓDULO: ' + estudante['modulo'])
+
+//criando uma cópia do objeto
+const estudanteCopia = { ...estudante }
+console.log(estudanteCopia)
+
+//alterar o nome da copia
+estudanteCopia.nome = 'Astrodev'
+console.log('NOME DO NOVO ESTUDANTE: ', estudanteCopia.nome)
+//incluir uma nova nota
+estudanteCopia.notasDoSemestre = [...estudanteCopia.notasDoSemestre, 9]
+console.log(estudanteCopia.notasDoSemestre)
+//alterar o módulo
+estudanteCopia['modulo'] = 'Introdução a WEB'
+console.log(estudanteCopia.modulo)
+
+const estudantesLabenu = []
+estudantesLabenu.push(estudante, estudanteCopia)
+console.log(estudantesLabenu)
